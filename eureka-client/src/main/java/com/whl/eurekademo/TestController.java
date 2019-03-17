39d60f1c607dfafeb4b6822c8b12829cda2821b8
WhlLong <whl9422@163.com>
2019-03-17 14:19:22 +0800

    添加eureka-client

--- eureka-client/src/main/java/com/whl/eurekademo/TestController.java
+++ eureka-client/src/main/java/com/whl/eurekademo/TestController.java
@@ -1,4 +1,15 @@
 package com.whl.eurekademo;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class TestController {
+
+    @GetMapping("test")
+    public String test(){
+        return "Hello World!";
+    }
+
 }
